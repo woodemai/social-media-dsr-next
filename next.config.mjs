5da3416cd9a6c83ea6f12f3559cdbb5bc255f26a
woodemai <n.savchenkoo73@gmail.com>
2024-05-30 12:32:18 +0300

    add back ppr

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    reactCompiler: true
+    ppr: true,
+    reactCompiler: true,
   },
   reactStrictMode: true,
 };
