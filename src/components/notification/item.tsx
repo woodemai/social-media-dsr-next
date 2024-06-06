38e70b990d1baa6fe38bde6afb00564d081dd4ee
woodemai <n.savchenkoo73@gmail.com>
2024-06-06 16:05:50 +0300

    Изменение списка уведомлений. (#31)
    Теперь, при принятии или отклонении запроса на подписку, уведомление автоматически убирается из списка.
    Также кнопка становятся disabled во время выполнения экшена

--- src/components/notification/item.tsx
+++ src/components/notification/item.tsx
@@ -4,33 +4,41 @@ import { Button } from '../ui/button';
 
 import { useToast } from '../ui/use-toast';
 
+import { useTransition } from 'react';
+
 import { subscribeAcceptAction, subscribeRejectAction } from '@/actions/user';
 import { FullSubscriptionRequest } from '@/data/subscription-request';
 
 interface NotificationItemProps {
   notification: FullSubscriptionRequest;
+  onNotificationRemove: (id: string) => void;
 }
 
-export const NotificationItem = ({ notification }: NotificationItemProps) => {
+export const NotificationItem = ({ notification, onNotificationRemove }: NotificationItemProps) => {
 
   const { toast } = useToast();
+  const [isPending, startTransition] = useTransition();
 
   const handleAccept = () => {
-    subscribeAcceptAction(notification).then(() => {
+    startTransition(async () => {
+      await subscribeAcceptAction(notification);
       toast({
         title: 'Принято',
         description: `Вы применяли подписку от ${notification.requestBy.name}`,
       });
+      onNotificationRemove(notification.id);
     });
   };
 
   const handleReject = () => {
-    subscribeRejectAction(notification).then(() => {
+    startTransition(async () => {
+      await subscribeRejectAction(notification);
       toast({
         title: 'Отклонено',
         description: `Вы отклонили запрос на подписку от ${notification.requestBy.name}`,
       });
-    });;
+      onNotificationRemove(notification.id);
+    });
   };
 
   return (
@@ -38,12 +46,14 @@ export const NotificationItem = ({ notification }: NotificationItemProps) => {
       <h4>{notification.requestBy.name} хочет на вас подписаться!</h4>
       <div className='space-x-4'>
         <Button
+          disabled={isPending}
           onClick={handleAccept}
           size='sm'
         >
           Принять
         </Button>
         <Button
+          disabled={isPending}
           onClick={handleReject}
           size='sm'
           variant='secondary'
--- src/components/notification/list.tsx
+++ src/components/notification/list.tsx
@@ -1,4 +1,9 @@
+'use client';
+
 import { NotificationItem } from './item';
+
+import { useState } from 'react';
+
 
 import { FullSubscriptionRequest } from '@/data/subscription-request';
 
@@ -6,12 +11,24 @@ interface NotificationListProps {
   notifications: FullSubscriptionRequest[];
 }
 
-export const NotificationList = ({ notifications }: NotificationListProps) => {
+export const NotificationList = ({ notifications: initialNotifications }: NotificationListProps) => {
+  const [notifications, setNotifications] = useState(initialNotifications);
+
+  const handleNotificationRemove = (id: string) => {
+    setNotifications(prev => prev.filter(notification => notification.id !== id));
+  };
+
+  if (notifications.length === 0) {
+    return (
+      <h1>Нет новых уведомлений</h1>
+    );
+  }
+
   return (
     <ul className='space-y-4 divide-y'>
       {notifications.map(notification => (
         <li key={notification.id}>
-          <NotificationItem notification={notification} />
+          <NotificationItem notification={notification} onNotificationRemove={handleNotificationRemove} />
         </li>
       ))}
     </ul>
