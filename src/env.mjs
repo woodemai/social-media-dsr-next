cf47ed0a04e53330e8050504242415b59233060e
woodemai <n.savchenkoo73@gmail.com>
2024-06-28 23:22:17 +0300

    Update env validation

--- src/env.mjs
+++ src/env.mjs
@@ -3,7 +3,7 @@ import { z } from 'zod';
 
 export const env = createEnv({
   server: {
-    NODE_ENV: z.enum(['development', 'test', 'production']),
+    NODE_ENV: z.enum(['development', 'preview', 'production']),
     DATABASE_URL: z.string().url(),
     DIRECT_URL: z.string().url(),
     AUTH_SECRET: z.string(),
